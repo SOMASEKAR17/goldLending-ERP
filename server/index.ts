fae72314acb6ef0eeb72a2906891f37e1fc502b6
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2026-03-13 23:56:53 +0530

    Fix: Explicit .js extensions for ESM compatibility on Vercel

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "./routes.js";
+import { setupVite, serveStatic, log } from "./vite.js";
 import dotenv from "dotenv";
 import connectPg from "connect-pg-simple";
-import { pool } from "./db";
+import { pool } from "./db.js";
 
 dotenv.config();
 
